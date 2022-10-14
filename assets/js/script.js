45473f7c51f3c90dbe8350de8093d81b3b3cac9d
Aspen92 <asplundchristofer@gmail.com>
2022-10-14 06:30:18 +0000

    Add intro text, game won/lost/draw text and restart button and a draw function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,30 +18,54 @@ document.addEventListener("DOMContentLoaded", () => {
     const computerChoice = document.getElementById('computer-choice');
     const playerScore = document.getElementById('player-score');
     const computerScore = document.getElementById('computer-score');
+    const winner = document.getElementById('winner');
+    const intro = document.getElementById('intro');
+
+    function setPlayerChoice(event) {
+        const index = event.target.dataset.index;
+        player1.choice = icons[index];
+        playerChoice.innerHTML = player1.choice;
+    }
+
+    function setComputerChoice() {
+        const rndInt = Math.floor(Math.random() * 4);
+        computer.choice = icons[rndInt];
+        computerChoice.innerHTML = computer.choice;
+    }
+
+    function hideIntroText() {
+        intro.style.opacity = 0;
+    };
+
 
     for (let button of buttons) {
         button.addEventListener("click", (event) => {
-            const index = event.target.dataset.index;
-            player1.choice = icons[index];
-            playerChoice.innerHTML = player1.choice;
+            setPlayerChoice(event);
+            setComputerChoice();
+            hideIntroText();
 
-            const rndInt = Math.floor(Math.random() * 4);
-            computer.choice = icons[rndInt];
-            computerChoice.innerHTML = computer.choice;
 
 
             if (player1.choice === "✊" && computer.choice === "✌️" || player1.choice === "✊" && computer.choice === "🤏") {
                 player1.score++;
+                winner.textContent = 'You Won!';
             } else if (player1.choice === "✋" && computer.choice === "✊" || player1.choice === "✋" && computer.choice === "🖖") {
                 player1.score++;
+                winner.textContent = 'You Won!';
             } else if (player1.choice === "✌️" && computer.choice === "✋" || player1.choice === "✌️" && computer.choice === "🤏") {
                 player1.score++;
+                winner.textContent = 'You Won!';
             } else if (player1.choice === "🤏" && computer.choice === "🖖" || player1.choice === "🤏" && computer.choice === "✋") {
                 player1.score++;
+                winner.textContent = 'You Won!';
             } else if (player1.choice === "🖖" && computer.choice === "✌️" || player1.choice === "🖖" && computer.choice === "✊") {
                 player1.score++;
+                winner.textContent = 'You Won!';
+            } else if (player1.choice === computer.choice) {
+                winner.textContent = 'Draw!'
             } else {
                 computer.score++;
+                winner.textContent = 'You Lost!';
             }
 
             playerScore.innerHTML = player1.score;
